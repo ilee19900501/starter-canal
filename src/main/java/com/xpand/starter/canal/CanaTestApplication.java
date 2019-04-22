6031459d3782c32fde631c564de684ad60ba8b22
liyunlong <liyunlong@1357.cn>
2019-04-22 13:02:49 +0800

    Initial commit

---
+++ src/main/java/com/xpand/starter/canal/CanaTestApplication.java
@@ -0,0 +1,14 @@
+package com.xpand.starter.canal;
+
+import com.xpand.starter.canal.annotation.EnableCanalClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableCanalClient
+public class CanaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanaTestApplication.class, args);
+	}
+}
